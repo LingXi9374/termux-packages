e9a88aa66e7aeeefa2b0481a558cd9e54c192e1d
LingXi9374 <140882027+LingXi9374@users.noreply.github.com>
2025-05-18 09:04:46 +0000

    addpkg(main/btop): Modern and colorful command line resource monitor

---
+++ packages/btop/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/aristocratos/btop
+TERMUX_PKG_DESCRIPTION="Modern and colorful command line resource monitor"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.4.3
+TERMUX_PKG_SRCURL=https://github.com/aristocratos/btop/releases/download/v${TERMUX_PKG_VERSION}/btop-aarch64-linux-musl.tbz
+TERMUX_PKG_SHA256=83137b2cbcb315fa17018a1666ff064def27303a3e30f3201f2e91cb20adc8a8
